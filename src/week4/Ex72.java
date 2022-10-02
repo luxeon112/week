16ed5c7c88e42b3dac65a60ffdf32dfd2365b2f1
Sander <sander.daskov@voco.ee>
2022-10-02 11:52:02 +0300

    Week 4 - Exercise 72.2: Your first money transfer

---
+++ src/week4/Ex72.java
@@ -0,0 +1,17 @@
+package week4;
+
+public class Ex72 {
+    public static void main(String[] args) {
+        Account mattAccount = new Account("Matt's account", 1000);
+        Account myAccount = new Account("My account", 0);
+
+        System.out.println(mattAccount);
+        System.out.println(myAccount);
+
+        mattAccount.withdrawal(100.0);
+        System.out.println(mattAccount);
+
+        myAccount.deposit(100.0);
+        System.out.println(myAccount);
+    }
+}
