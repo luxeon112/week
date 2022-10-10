ee4a85c988644a9b37d6debc946272384e713808
Sander <sander.daskov@voco.ee>
2022-10-10 20:54:24 +0300

    Week 9 - Exercise 20.3: AverageSensor

--- src/week9/Ex20.java
+++ src/week9/Ex20.java
@@ -1,24 +1,25 @@
 package week9;
 
+import week9.measurements.AverageSensor;
 import week9.measurements.ConstantSensor;
 import week9.measurements.Sensor;
 import week9.measurements.Thermometer;
 
 public class Ex20 {
     public static void main(String[] args) {
-        ConstantSensor ten = new ConstantSensor(10);
-        ConstantSensor minusFive = new ConstantSensor(-5);
-
-        System.out.println( ten.measure() );
-        System.out.println( minusFive.measure() );
-
-        System.out.println( ten.isOn() );
-        ten.off();
-        System.out.println( ten.isOn() );
         Sensor kumpula = new Thermometer();
         kumpula.on();
         System.out.println("the temperature in Kumpula is "+kumpula.measure() + " degrees");
-        kumpula.off();
-        System.out.println("the temperature in Kumpula is "+kumpula.measure() + " degrees");
+
+        Sensor kaisaniemi = new Thermometer();
+        Sensor helsinkiVantaa = new Thermometer();
+
+        AverageSensor helsinkiArea = new AverageSensor();
+        helsinkiArea.addSensor(kumpula);
+        helsinkiArea.addSensor(kaisaniemi);
+        helsinkiArea.addSensor(helsinkiVantaa);
+
+        helsinkiArea.on();
+        System.out.println("the temperature in Helsinki area is "+helsinkiArea.measure() + " degrees");
     }
 }
