9061071f5a9190cd404ecb4ca2211223659c1be4
Sander <sander.daskov@voco.ee>
2022-10-10 18:28:12 +0300

    Week 6 - Exercise 100.3: Counting the number of stars

---
+++ src/week6/Ex100.java
@@ -0,0 +1,13 @@
+public class Ex100 {
+
+    public static void main(String[] args) {
+
+        NightSky NightSky = new NightSky(200, 10);
+        NightSky.print();
+        System.out.println("Number of stars: " + NightSky.starsInLastPrint());
+        System.out.println("");
+
+        NightSky.print();
+        System.out.println("Number of stars: " + NightSky.starsInLastPrint());
+    }
+}
