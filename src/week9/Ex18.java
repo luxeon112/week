0738727f0c36d382c05fdcc6b5856b8a9b455955
Sander <sander.daskov@voco.ee>
2022-10-10 20:14:36 +0300

    Week 9 - Exercise 18.4: Packing Items

--- src/week9/Ex18.java
+++ src/week9/Ex18.java
@@ -1,7 +1,8 @@
-package week9;
-
 import week9.mooving.domain.Item;
 import week9.mooving.domain.Thing;
+import week9.mooving.domain.Box;
+import week9.mooving.logic.Packer;
+
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -13,8 +14,27 @@ public class Ex18 {
         items.add(new Item("passport", 2));
         items.add(new Item("toothbrash", 1));
         items.add(new Item("circular saw", 100));
+        // the things we want to pack
+        List<Thing> things = new ArrayList<Thing>();
+        things.add(new Item("passport", 2));
+        things.add(new Item("toothbrash", 1));
+        things.add(new Item("book", 4));
+        things.add(new Item("circular saw", 8));
+        things.add(new Item("passport", 2));
+
+        // we create a packer which uses boxes whose valume is 10
+        Packer packer = new Packer(10);
+
+        // we ask our packer to pack things into boxes
+        List<Box> boxes = packer.packThings( things );
+
+        System.out.println("number of boxes: "+boxes.size());
 
         Collections.sort(items);
         System.out.println(items);
+        for (Box box : boxes) {
+            System.out.println("  things in the box: "+box.getVolume()+" dm^3");
+            System.out.println(box);
+        }
     }
 }
