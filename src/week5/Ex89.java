8da5037e258778cf50897279fa91d67b326ad3b5
Sander <sander.daskov@voco.ee>
2022-10-10 17:58:03 +0300

    Week 5 - Exercise 89 Clock Object

---
+++ src/week5/Ex89.java
@@ -0,0 +1,12 @@
+public class Ex89 {
+    public static void main(String[] args) {
+        Clock clock = new Clock(23, 59, 50);
+
+        int i = 0;
+        while (i < 20) {
+            System.out.println(clock);
+            clock.tick();
+            i++;
+        }
+    }
+}
