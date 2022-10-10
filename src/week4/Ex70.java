69a25433737e524b69afb16a5219d27137e8ff04
Sander <sander.daskov@voco.ee>
2022-10-10 17:35:29 +0300

    Week 4 - Exercise 70 Combining Array Lists

---
+++ src/week4/Ex70.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Ex70 {
+
+    public static void main(String[] args) {
+
+
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
+
+        Collections.addAll(list1, 4, 3);
+
+
+
+        Collections.addAll(list2, 5, 10, 7);
+
+        System.out.println(list1);
+        System.out.println(list2);
+
+
+    }
+
+    public static void combine(ArrayList<Integer> first, ArrayList<Integer> second) {
+        first.addAll(second);
+    }
+
+}
